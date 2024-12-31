d49c27ddb04e95797000c74de1624eca8347608f
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-31 20:45:59 +0600

    object method created

---
+++ object.js
@@ -0,0 +1,12 @@
+const student = {
+  age: 23,
+  roll: 5,
+  subject: ['math', 'english', 'physics', 'chemistry'],
+  'is Money': 20000
+}
+// console.log(student['subject'][2])
+
+student.age = 25;
+student['subject'] = 'biology'
+console.log(student)
+
